f704e20e97f582a410089e3b4732a92d85baabdf
D0V4HKIIN <jonas.bonnaudet@gmail.com>
2025-02-05 19:27:37 +0100

    with user :

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ stage = 1
 direction = "Forward"
 weather = "Dry"
 group = "60s"
-filter = 0
+filter = 7
 platform = 2
+user = 76561198230518420
 
 r = requests.get(f"https://www.funselektorfun.com/artofrally/leaderboard/{area}_Stage_{
-                 stage}_{direction}_{weather}_{group}/{filter}/{platform}")
+                 stage}_{direction}_{weather}_{group}/{filter}/{platform}/{user}")
 # /{user}/{friends}
 print(r.text)
