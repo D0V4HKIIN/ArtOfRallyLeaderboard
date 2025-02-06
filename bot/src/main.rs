268923e3381363c499aecc5c2c552a89910071f9
D0V4HKIIN <jonas.bonnaudet@gmail.com>
2025-02-06 23:04:52 +0100

    save to file

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,56 +1,116 @@
 extern crate reqwest;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Write;
 fn main() {
     println!("Hello, world!");
+    let area = Area::Finland;
+    let stage = 1;
+    let direction = Direction::Forward;
+    let weather = Weather::Dry;
+    let group = Group::Sixties;
+    let filter = Filter::Friends;
+    let platform = Platform::Steam;
     let entries = get_leaderboard_entries(
-        Area::Finland,
-        1,
-        Direction::Forward,
-        Weather::Dry,
-        Group::Sixties,
-        Filter::Friends,
-        Platform::Steam,
+        &area,
+        stage,
+        &direction,
+        &weather,
+        &group,
+        &filter,
+        &platform,
         76561198230518420,
-        vec![76561198087789780, 2],
+        &vec![76561198087789780, 76561198062269100],
     );
 
     println!("{:#?}", entries);
+
+    // save_entries(&entries, &area, stage, &direction, &weather, &group);
+    // println!(
+    //     "{:#?}",
+    //     read_entries(&area, stage, &direction, &weather, &group).unwrap()
+    // );
 }
 
-fn get_leaderboard_entries(
-    area: Area,
+fn stage_string(
+    area: &Area,
     stage: usize, // should be 1,2,3,4,5,6
-    direction: Direction,
-    weather: Weather,
-    group: Group,
-    filter: Filter,
-    platform: Platform,
-    user: u64,
-    friends: Vec<u64>,
-) -> Vec<LeaderboardEntry> {
+    direction: &Direction,
+    weather: &Weather,
+    group: &Group,
+) -> String {
     let area_name = area.value();
     let direction_name = direction.value();
     let weather_name = weather.value();
     let group_name = group.value();
-    let filter_index = filter as isize;
-    let platform_index = platform as isize;
 
-    let friend_list: String = format!("{:?}", friends);
+    return format!("{area_name}_Stage_{stage}_{direction_name}_{weather_name}_{group_name}");
+}
 
-    let body = reqwest::blocking::get(format!("https://www.funselektorfun.com/artofrally/leaderboard/{area_name}_Stage_{stage}_{direction_name}_{weather_name}_{group_name}/{filter_index}/{platform_index}/{user}/{friend_list}")).unwrap().text().unwrap();
+fn get_leaderboard_entries(
+    area: &Area,
+    stage: usize, // should be 1,2,3,4,5,6
+    direction: &Direction,
+    weather: &Weather,
+    group: &Group,
+    filter: &Filter,
+    platform: &Platform,
+    user: u64,
+    friends: &Vec<u64>,
+) -> Vec<LeaderboardEntry> {
+    let stage_string = stage_string(area, stage, direction, weather, group);
+    let filter_index = filter.value();
+    let platform_index = platform.value();
+
+    let friend_list: String = format!("{:?}", friends).split_whitespace().collect();
+
+    let body = reqwest::blocking::get(format!("https://www.funselektorfun.com/artofrally/leaderboard/{stage_string}/{filter_index}/{platform_index}/{user}/{friend_list}")).unwrap().text().unwrap();
 
     let result: Response = serde_json::from_str(&body).expect("response is not well-formated");
 
     return result.leaderboard;
 }
 
+fn save_entries(
+    entries: &Vec<LeaderboardEntry>,
+    area: &Area,
+    stage: usize, // should be 1,2,3,4,5,6
+    direction: &Direction,
+    weather: &Weather,
+    group: &Group,
+) -> () {
+    let json_entries = serde_json::to_string_pretty(&entries)
+        .expect(&format!("failed to serialize entries {:?}", entries));
+
+    let mut file = File::create(stage_string(area, stage, direction, weather, group))
+        .expect("failed to create stage file");
+    file.write_all(json_entries.as_bytes())
+        .expect("failed to write to stage file");
+}
+
+fn read_entries(
+    area: &Area,
+    stage: usize, // should be 1,2,3,4,5,6
+    direction: &Direction,
+    weather: &Weather,
+    group: &Group,
+) -> Result<Vec<LeaderboardEntry>, serde_json::Error> {
+    let file = File::open(stage_string(area, stage, direction, weather, group))
+        .expect("can't open stage file");
+
+    serde_json::from_reader(file)
+}
+
+// result will never be used but is needed for json parsing
+#[allow(dead_code)]
 #[derive(Debug, Deserialize)]
 struct Response {
     leaderboard: Vec<LeaderboardEntry>,
     result: isize, // idk what this value does
 }
 
-#[derive(Debug, Deserialize)]
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
 struct LeaderboardEntry {
     uniqueID: u64, // unsure
     userName: String,
@@ -144,16 +204,33 @@ impl Group {
 
 // idk what i am doing with all theses numbers :))
 enum Filter {
-    Top = 0,
-    AroundMe = 1,
-    Number = 2,
-    Count = 3,
-    PlayerRank = 4,
-    NumberOne = 5,
-    Friends = 6,
-    OnlyMe = 7,
+    Top,
+    AroundMe,
+    Number,
+    Count,
+    PlayerRank,
+    NumberOne,
+    Friends,
+    OnlyMe,
 }
 
+impl Filter {
+    fn value(&self) -> usize {
+        match *self {
+            Filter::Top => 0,
+            Filter::AroundMe => 1,
+            Filter::Number => 2,
+            Filter::Count => 3,
+            Filter::PlayerRank => 4,
+            Filter::NumberOne => 5,
+            Filter::Friends => 6,
+            Filter::OnlyMe => 7,
+        }
+    }
+}
+
+// realistically we only care about steam
+#[allow(dead_code)]
 enum Platform {
     Epic = 0,
     Gog = 1,
@@ -162,4 +239,18 @@ enum Platform {
     Playstation = 4,
     Nintendo = 5,
     None = 6,
+}
+
+impl Platform {
+    fn value(&self) -> usize {
+        match *self {
+            Platform::Epic => 0,
+            Platform::Gog => 1,
+            Platform::Steam => 2,
+            Platform::Xbox => 3,
+            Platform::Playstation => 4,
+            Platform::Nintendo => 5,
+            Platform::None => 6,
+        }
+    }
 }
